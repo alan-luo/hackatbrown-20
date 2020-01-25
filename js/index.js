00179eb35dbeb397379e73cfbc41894b304fd35a
Jannie <jz3191@columbia.edu>
2020-01-25 15:34:44 -0500

    added colors!

--- js/index.js
+++ js/index.js
@@ -64,17 +64,18 @@ class RandomShape extends Component {
 	vertices = 0;
 	xVal = null;
 	yVal = null;
+	endVert = 0;
 	constructor(pos, args){
 		super(pos, {});
 		this.vertices = args.vertices;
 		this.xVal = new Array(this.vertices);
 		this.yVal = new Array(this.vertices);
-
+		this.endVert = 4+Math.round(Math.random())*(this.vertices-5);
 
 		for (let i = 0; i < this.vertices; i++) {
-			let radius = 50 + Math.random()*100;
-			this.xVal[i] = Math.cos((360*i/this.vertices)*Math.PI/180)*radius;
-			this.yVal[i] = Math.sin((360*i/this.vertices)*Math.PI/180)*radius;
+			let radius = 50 + Math.random()*30;
+			this.xVal[i] = this.pos.x + Math.cos((360*i/this.vertices)*Math.PI/180)*radius;
+			this.yVal[i] = this.pos.y + Math.sin((360*i/this.vertices)*Math.PI/180)*radius;
 		}
 		
 	}
@@ -82,14 +83,68 @@ class RandomShape extends Component {
 	draw = function(){
 
 		ctx.beginPath();
-		for (let j = 0; j < 10; j++) {
+		for (let j = 0; j < this.vertices; j++) {
 			ctx.lineTo(this.xVal[j], this.yVal[j]);
 		}
 		ctx.closePath();
 		ctx.stroke();
 
+		// ctx.lineTo(this.xVal[Math.trunc(this.vertices/4)],this.yVal[Math.trunc(this.vertices/4)])
+		// ctx.lineTo(this.xVal[Math.trunc(3*this.vertices/4)],this.yVal[Math.trunc(3*this.vertices/4)])
+
+
+		ctx.beginPath();
+		ctx.lineTo(this.xVal[0], this.yVal[0]);
+		ctx.lineTo(this.xVal[2],this.yVal[2]);
+		ctx.lineTo(this.xVal[this.endVert],this.yVal[this.endVert]);
+		ctx.stroke();
+		ctx.closePath();
+
+
+
+		ctx.beginPath();
+		ctx.lineTo(this.xVal[0], this.yVal[0]);
+		for (let j = this.vertices-1; j >= this.endVert; j--){
+			ctx.lineTo(this.xVal[j], this.yVal[j]);
+		}
+		ctx.lineTo(this.xVal[2],this.yVal[2]);
+		ctx.lineTo(this.xVal[0], this.yVal[0]);
+		ctx.closePath();
+		ctx.fillStyle = 'green';
+		ctx.fill();
+
+		ctx.beginPath();
+		ctx.lineTo(this.xVal[0], this.yVal[0]);
+		for (let j = 1; j <= 2; j++){
+			ctx.lineTo(this.xVal[j], this.yVal[j]);
+		}
+		ctx.lineTo(this.xVal[0], this.yVal[0]);
+		ctx.closePath();
+		ctx.fillStyle = 'steelblue';
+		ctx.fill();
+
+		ctx.beginPath();
+		ctx.lineTo(this.xVal[2], this.yVal[2]);
+		for (let j = 3; j <= this.endVert; j++){
+			ctx.lineTo(this.xVal[j], this.yVal[j]);
+		}
+		ctx.lineTo(this.xVal[2], this.yVal[2]);
+		ctx.closePath();
+		ctx.fillStyle = 'skyblue';
+		ctx.fill();
+
+
 	}
 
+}
+
+function isPrime(value) {
+    for(var i = 2; i < value; i++) {
+        if(value % i === 0) {
+            return false;
+        }
+    }
+    return value > 1;
 }
 
 
@@ -113,6 +168,14 @@ function loop() {
 }
 
 //components.push(makeComponent("box", {x:0, y:0}));
-components.push(new RandomShape({x:0, y: 0}, {vertices: 10}));
-components.push(new Box({x:0, y:0}, {width: 50}));
+//components.push(new RandomShape({x:0, y: 0}, {vertices: 10}));
+components.push(new RandomShape({x:0, y: 0}, {vertices: 8}));
+components.push(new RandomShape({x:0, y: 0}, {vertices: 6}));
+
+components.push(new RandomShape({x:0, y: 0}, {vertices: 7}));
+components.push(new RandomShape({x:0, y: 0}, {vertices: 5}));
+// components.push(new RandomShape({x:+125, y: -125}, {vertices: 16}));
+// components.push(new RandomShape({x:-125, y: +125}, {vertices: 22}));
+
+//components.push(new Box({x:0, y:0}, {width: 50}));
 setup();
