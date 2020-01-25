53f8d97020cba14a2cbe8fae272aa32f1e058695
Alan Luo <me@alanluo.com>
2020-01-25 12:41:43 -0500

    some canvas stuff

--- js/index.js
+++ js/index.js
@@ -1,4 +1,28 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
-ctx.fillRect(0, 0, 50, 50);
+
+const [CANVAS_WIDTH, CANVAS_HEIGHT] = [800, 600];
+
+let frames = 0;
+function setup() {
+
+	loop();
+}
+let range = document.getElementById('range');
+function loop() {
+	ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+	let grd = ctx.createLinearGradient(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+	grd.addColorStop(0, `hsl(${(range.value/100)*255}, 40%, 90%)`);
+	grd.addColorStop(1, `hsl(${(range.value/100)*255}, 70%, 60%)`);
+	// x++;
+
+	ctx.fillStyle=grd;
+	ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+
+	window.requestAnimationFrame(loop);
+}
+
+setup();
