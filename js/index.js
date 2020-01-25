c8a7174cce1001fb884593a43da6b008a7589716
Alan Luo <me@alanluo.com>
2020-01-25 14:24:05 -0500

    pushed makePath (and PolyBlob)

--- js/index.js
+++ js/index.js
@@ -25,6 +25,50 @@ class Box extends Component {
 		ctx.fillRect(this.pos.x, this.pos.y, this.width, this.width);
 	}
 }
+/** usage: adds the list of vertices to the working path
+  - vertices: [{x:, y:,}, {x:, y:,}, ...]
+  - closed: bool (when true, adds first vertex to end of path)
+
+  - note: doesn't fill or draw anything (do this yourself) 
+**/
+function makePath(vertices, closed) {
+	ctx.beginPath();
+	ctx.moveTo(vertices[0].x, vertices[0].y);
+
+	for(let i=1; i<vertices.length; i++) {
+		ctx.lineTo(vertices[i].x, vertices[i].y)
+	} 
+	if(closed) {
+		let myV = vertices[vertices.length-1];
+		ctx.lineTo(myV.x, myV.y);
+	}
+}
+class PolyBlob extends Component {
+	// outerVerts = [];
+	// innerVerts = [];
+	// polys = [];
+
+	constructor(pos, args) {
+		super(pos, {});
+
+		this.outerVerts = [];
+		this.innerVerts = [];
+		this.polys = [];
+
+		let numVerts = 7;
+		for(let i=0; i<numVerts; i++) {
+			let angle = (2*Math.PI / numVerts)*i;
+			this.outerVerts.push({
+				x: 50*Math.cos(angle),
+				y: 50*Math.sin(angle),
+			});
+		}
+	}
+	draw = function() {
+		makePath(this.outerVerts, true);
+		ctx.fill();
+	}
+}
 
 function setup() {
 	ctx.translate(canvas.width/2, canvas.height/2);
@@ -45,5 +89,5 @@ function loop() {
 	window.requestAnimationFrame(loop);
 }
 
-components.push(new Box({x:0, y:0}, {width: 50}));
+components.push(new PolyBlob({x:0, y:0}, {width: 50}));
 setup();
