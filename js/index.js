51fa94a8cbc7834fadb9aa7f889e62c2df2d46c0
Alan Luo <me@alanluo.com>
2020-01-25 12:56:00 -0500

    updated thing

--- js/index.js
+++ js/index.js
@@ -1,26 +1,30 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
-console.log(canvas.width, canvas.height);
 
 let components = [];
-function makeComponent() {
+function makeComponent(myProps) {
 	return ({
-		props: {
-			x: 100,
-			y: 100,
-		},
+		type: "box",
+		props: myProps,
 		draw: function() {
-
+			ctx.fillRect(this.props.x, this.props.y, 50, 50);
 		},
 	});
 }
 
+for(let i=0; i<10; i++) {
+	myComponent = makeComponent({
+		x:Math.random()*700,
+		y:Math.random()*500
+	});
+
+	components.push(myComponent);
+}
 
 function setup() {
 	loop();
 }
-
 function loop() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
