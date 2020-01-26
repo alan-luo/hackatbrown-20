09518e40ae1d33f8c6bd9d2538226c69e9070828
Alan Luo <me@alanluo.com>
2020-01-25 22:08:44 -0500

    added orientation to squareearring

--- js/SquareEarring.js
+++ js/SquareEarring.js
@@ -1,36 +1,41 @@
 class SquareEarring extends Component {
     width = 0;
     angle = 0;
+
+    orientation = 0;
     constructor(pos, args) {
 		super(pos, {});
 		this.width = args.width;
 		this.angle = args.angle;
+
+		this.orientation = Math.floor(Math.random()*4);
 	}
 	update = function() {
 		// this.angle=Math.PI*0.5*Math.sin(this.frames*0.005);
 	};
 	draw = function() {
 		ctx.save();
-		ctx.translate(-this.width/2, -this.width/2);
+		// ctx.translate(-this.width/2, -this.width/2);
+		ctx.rotate(Math.PI*0.5*this.orientation);
 
 	    this.resetFill();
 
 	    ctx.beginPath();
-		ctx.rect(0, 0, this.width, this.width);
+		ctx.rect(-this.width/2, -this.width/2, this.width, this.width);
 		this.fill();
 
 		let sin = Math.sin(this.angle)*this.width;
 		let cos = Math.cos(this.angle)*this.width;
 
 		//inner lines
-		ctx.moveTo(0, 0);
-        ctx.lineTo(sin, this.width);
+		ctx.moveTo(-this.width/2, -this.width/2);
+        ctx.lineTo(sin - this.width/2, this.width/2);
         this.fill();
 
-        ctx.lineTo(this.width, sin);
+        ctx.lineTo(this.width/2, sin - this.width/2);
         this.fill();
 
-        ctx.lineTo(this.width - sin, 0);
+        ctx.lineTo(this.width/2 - sin, -this.width/2);
 
         // ctx.fillStyle = colors.getFill();
         this.fill();
